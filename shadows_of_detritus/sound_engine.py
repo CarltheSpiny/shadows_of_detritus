d43ffdff69582e74489f82bef077e4b78463d035
CarltheSpiny <60937275+CarltheSpiny@users.noreply.github.com>
2020-11-14 01:52:26 -0800

    Delete sound_engine.py

--- shadows_of_detritus/sound_engine.py
+++
@@ -1,9 +0,0 @@
-import pygame
-
-pygame.init()
-
-background_music_1 = pygame.mixer.Sound("assets/sounds/dev_music/earth.ogg")
-
-debug = pygame.mixer.Sound("assets/sounds/dev_music/winters_white.ogg")
-
-debug_menu = pygame.mixer.Sound("assets/sounds/dev_music/main_theme.ogg")
