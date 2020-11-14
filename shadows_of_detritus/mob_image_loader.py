eef7eb0488f656c0a4e308d62ffe2b5b144d3610
CarltheSpiny <60937275+CarltheSpiny@users.noreply.github.com>
2020-11-14 01:51:39 -0800

    Delete mob_image_loader.py

--- shadows_of_detritus/mob_image_loader.py
+++
@@ -1,8 +0,0 @@
-import pygame
-
-pygame.init()
-
-mushroom = pygame.image.load('assets/textures/dev_enemy/mushroom_idle1.png')
-
-mushroom_running = pygame.image.load('assets/textures/dev_enemy/mushroom_ew_1.png',
-                                     'assets/textures/dev_enemy/mushroom_ew_2.png')
