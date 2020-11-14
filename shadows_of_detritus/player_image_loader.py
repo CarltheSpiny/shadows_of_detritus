fddcba0d8e499b073c269838ba2735b7b7b2f3f0
CarltheSpiny <60937275+CarltheSpiny@users.noreply.github.com>
2020-11-14 02:03:16 -0800

    Revert "Assets"

---
+++ shadows_of_detritus/player_image_loader.py
@@ -0,0 +1,58 @@
+import pygame
+
+pygame.init()
+
+walkRight = [pygame.image.load('assets/textures/dev_player/r1.png'),
+             pygame.image.load('assets/textures/dev_player/r2.png')]
+
+"""[pygame.image.load('assets/textures/player/kyle/kyle_u1.png'),
+             pygame.image.load('assets/textures/player/kyle/kyle_u2.png'),
+             pygame.image.load('assets/textures/player/kyle/kyle_u3.png'),
+             pygame.image.load('assets/textures/player/kyle/kyle_u4.png'),
+             pygame.image.load('assets/textures/player/kyle/kyle_u5.png'),
+             pygame.image.load('assets/textures/player/kyle/kyle_u6.png'),
+             pygame.image.load('assets/textures/player/kyle/kyle_u8.png'),
+             pygame.image.load('assets/textures/player/kyle/kyle_u9.png')]"""
+
+walkLeft = [pygame.image.load('assets/textures/dev_player/l1.png'),
+            pygame.image.load('assets/textures/dev_player/l2.png')]
+
+walkUp = [pygame.image.load('assets/textures/dev_player/u1.png'),
+          pygame.image.load('assets/textures/dev_player/u2.png')]
+
+walkDown = [pygame.image.load('assets/textures/dev_player/d1.png'),
+            pygame.image.load('assets/textures/dev_player/d2.png')]
+
+"""walkDownRight = [pygame.image.load('assets/textures/dev_player/d_r1.png'),
+                 pygame.image.load('assets/textures/dev_player/d_r2.png')]
+
+walkDownLeft = [pygame.image.load('assets/textures/dev_player/d_l1.png'),
+                pygame.image.load('assets/textures/dev_player/d_l2.png')]"""
+
+walkUpRight = [pygame.image.load('assets/textures/dev_player/u_r1.png'),
+               pygame.image.load('assets/textures/dev_player/u_r2.png')]
+
+walkUpLeft = [pygame.image.load('assets/textures/dev_player/u_l1.png'),
+              pygame.image.load('assets/textures/dev_player/u_l2.png')]
+
+dashRight = [pygame.image.load('assets/textures/dev_player/dash_r1.png'),
+             pygame.image.load('assets/textures/dev_player/dash_r2.png')]
+
+dashUpRight = [pygame.image.load('assets/textures/dev_player/dash_u_r1.png'),
+               pygame.image.load('assets/textures/dev_player/dash_u_r2.png')]
+
+dashLeft = [pygame.image.load('assets/textures/dev_player/dash_l1.png'),
+            pygame.image.load('assets/textures/dev_player/dash_l2.png')]
+
+dashUpLeft = [pygame.image.load('assets/textures/dev_player/dash_u_l1.png'),
+               pygame.image.load('assets/textures/dev_player/dash_u_l2.png')]
+
+dashUp = [pygame.image.load('assets/textures/dev_player/dash_u1.png'),
+          pygame.image.load('assets/textures/dev_player/dash_u4.png'),
+          pygame.image.load('assets/textures/dev_player/dash_u2.png'),
+          pygame.image.load('assets/textures/dev_player/dash_u3.png')]
+
+dashDown = [pygame.image.load('assets/textures/dev_player/dash_d1.png'),
+            pygame.image.load('assets/textures/dev_player/dash_d4.png'),
+            pygame.image.load('assets/textures/dev_player/dash_d2.png'),
+            pygame.image.load('assets/textures/dev_player/dash_d3.png')]
--- tile_maps/debug.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="debug" tilewidth="32" tileheight="32" spacing="1" tilecount="848" columns="16">
- <image source="../textures/debug_tile_sheet/debug.png" width="529" height="1750"/>
-</tileset>
